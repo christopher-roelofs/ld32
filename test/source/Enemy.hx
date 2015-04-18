c84541c9fe6ad93e9f77846a2163ff2f3c7c2ec0
Croelofs <christopher.roelofs@gmail.com>
2015-04-18 13:32:52 -0400

    level update

--- test/source/Enemy.hx
+++ test/source/Enemy.hx
@@ -10,113 +10,69 @@ import flixel.util.FlxVelocity;
 
 class Enemy extends FlxSprite
 {
-	public var speed:Float = 80;
-	public var etype(default, null):Int;
-	private var _brain:FSM;
-	private var _idleTmr:Float;
-	private var _moveDir:Float;
-	public var seesPlayer:Bool = false;
-	public var playerPos(default, null):FlxPoint;
+    public var speed:Float = 140;
+    public var etype(default, null):Int;
 	
-	public function new(X:Float=0, Y:Float=0, EType:Int) 
-	{
-		super(X, Y);
-		etype = EType;
-		loadGraphic("assets/images/enemy-" + Std.string(etype) + ".png", true, 16, 16);
-		setFacingFlip(FlxObject.LEFT, false, false);
-		setFacingFlip(FlxObject.RIGHT, true, false);
-		animation.add("d", [0, 1, 0, 2], 6, false);
-		animation.add("lr", [3, 4, 3, 5], 6, false);
-		animation.add("u", [6, 7, 6, 8], 6, false);
-		drag.x = drag.y = 10;
-		width = 8;
-		height = 14;
-		offset.x = 4;
-		offset.y = 2;
-		_brain = new FSM(idle);
-		_idleTmr = 0;
-		playerPos = FlxPoint.get();
-	}
+
+    public function new(X:Float=0, Y:Float=0, EType:Int) 
+    {
+        super(X, Y);
+        etype = EType;
+        loadGraphic("assets/images/enemy-" + Std.string(etype) + ".png", true, 16, 16);
+        setFacingFlip(FlxObject.LEFT, false, false);
+        setFacingFlip(FlxObject.RIGHT, true, false);
+        animation.add("d", [0, 1, 0, 2], 6, false);
+        animation.add("lr", [3, 4, 3, 5], 6, false);
+        animation.add("u", [6, 7, 6, 8], 6, false);
+        drag.x = drag.y = 10;
+        width = 8;
+        height = 14;
+        offset.x = 4;
+        offset.y = 2;
+    }
 	
-	override public function update():Void 
-	{
-		_brain.update();
-		super.update();
-	}
-	
-	public function idle():Void
-	{
-		if (seesPlayer)
+	private function checkEnemyVision(e:Enemy):Void
 		{
-			_brain.activeState = chase;
-		}
-		else if (_idleTmr <= 0)
-		{
-			if (FlxRandom.chanceRoll(1))
-			{
-				_moveDir = -1;
-				velocity.x = velocity.y = 0;
-			}
+			if (_mWalls.ray(e.getMidpoint(), _player.getMidpoint()))
+				{
+					e.seesPlayer = true;
+					e.playerPos.copyFrom(_player.getMidpoint());
+				}
 			else
-			{
-				_moveDir = FlxRandom.intRanged(0, 8) * 45;
-				FlxAngle.rotatePoint(speed * .5, 0, 0, 0, _moveDir, velocity);
-				
-			}
-			_idleTmr = FlxRandom.intRanged(1, 4);			
+				e.seesPlayer = false;
 		}
-		else
-			_idleTmr -= FlxG.elapsed;
-		
-	}
-	
-	public function chase():Void
-	{
-		if (!seesPlayer)
-		{
-			_brain.activeState = idle;
-		}
-		else
-		{
-			FlxVelocity.moveTowardsPoint(this, playerPos, Std.int(speed));
-		}
-	}
-	
-	
-	override public function draw():Void 
-	{
-		if (velocity.x != 0 || velocity.y != 0)
-		{
-			
-			if (Math.abs(velocity.x) > Math.abs(velocity.y))
-			{
-				if (velocity.x < 0)
-					facing = FlxObject.LEFT;
-				else
-					facing = FlxObject.RIGHT;
-			}
-			else
-			{
-				if (velocity.y < 0)
-					facing = FlxObject.UP;
-				else
-					facing = FlxObject.DOWN;
-			}
-			
-			switch(facing)
-			{
-				case FlxObject.LEFT, FlxObject.RIGHT:
-					animation.play("lr");
-					
-				case FlxObject.UP:
-					animation.play("u");
-					
-				case FlxObject.DOWN:
-					animation.play("d");
-			}
-		}
-			
-		super.draw();
-	}
-	
+
+    override public function draw():Void 
+    {
+        if ((velocity.x != 0 || velocity.y != 0 ) && touching == FlxObject.NONE)
+        {
+            if (Math.abs(velocity.x) > Math.abs(velocity.y))
+            {
+                if (velocity.x < 0)
+                    facing = FlxObject.LEFT;
+                else
+                    facing = FlxObject.RIGHT;
+            }
+            else
+            {
+                if (velocity.y < 0)
+                    facing = FlxObject.UP;
+                else
+                    facing = FlxObject.DOWN;
+            }
+
+            switch(facing)
+            {
+                case FlxObject.LEFT, FlxObject.RIGHT:
+                    animation.play("lr");
+
+                case FlxObject.UP:
+                    animation.play("u");
+
+                case FlxObject.DOWN:
+                    animation.play("d");
+            }
+        }
+        super.draw();
+    }
 }
